ce957b8b9b6d771a999094360d7427c0ce358ac4
cocvu <cocvu@nightcord.unknown.tld>
2025-05-25 05:19:37 +0000

    add nvcc support and extra-substituters

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "Nix flake that setup CUDA devshell";
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -13,15 +20,22 @@
           system = system;
           config = {
             allowUnfree = true;
+            cudaSupport = true;
           };
         };
       in {
         devShell = pkgs.mkShell {
-          shellHook = ''
-            export CUDA_PATH=${pkgs.cudatoolkit}
-            export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
-            export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+          buildInputs = with pkgs; [
+            cudaPackages.cudatoolkit
+            linuxPackages.nvidia_x11
+            ncurses5
+          ];
+          shellHook = with pkgs; ''
+            export CUDA_PATH=${cudatoolkit}
+            # export LD_LIBRARY_PATH=/usr/lib/wsl/lib:${linuxPackages.nvidia_x11}/lib:${ncurses5}/lib
+            export EXTRA_LDFLAGS="-L/lib -L${linuxPackages.nvidia_x11}/lib"
             export EXTRA_CCFLAGS="-I/usr/include"
+            alias nvcc='nvcc --cudadevrt none --cudart shared'
           '';
         };
     });
