c0d7e8e99b93c0ad40a2a82fcac01bbb19127097
cocvu <cocvu@nightcord.unknown.tld>
2025-06-22 16:10:20 +0000

    use nixpkgs stable to reuse build cache

--- flake.nix
+++ flake.nix
@@ -10,13 +10,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs-stable, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
+        pkgs = import nixpkgs-stable {
           system = system;
           config = {
             allowUnfree = true;
